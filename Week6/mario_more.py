f96c17b34eba03fb71997f71185e9214b8966bc4
Riley Hu <108101131+hxr413@users.noreply.github.com>
2023-03-07 01:39:53 +0000

    Add files via upload

---
+++ Week6/mario_more.py
@@ -0,0 +1,30 @@
+# TODO
+
+from cs50 import get_int
+
+
+def main():
+    # ask user to input height
+    height = get_int('Height: ')
+
+    if height < 1 or height > 8:
+        print('Please input a positive integer between 1 and 8, inclusive.')
+        # restart the function, so that if user input a valid height, the program will continue to print
+        main()
+    else:
+        # print the pyramid from i = 1 to height
+        for i in range(1, height + 1):
+            # there should be j spaces where j = height - i
+            for j in range(height - i):
+                print(' ', end="")
+            for k in range(i):
+                print('#', end="")
+            # print the two spaces in between
+            print('  ', end="")
+            # simply repeat to print the same numebr of hashes
+            for k in range(i):
+                print('#', end="")
+            print()
+
+
+main()
