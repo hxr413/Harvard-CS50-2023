789a7da40a8d5efa6c2cc7e30a9a9cf9b05cdb9b
Riley Hu <108101131+hxr413@users.noreply.github.com>
2023-03-08 02:18:07 +0000

    Add files via upload

---
+++ Week6/jar.py
@@ -0,0 +1,46 @@
+class Jar:
+    def __init__(self, capacity=12):
+        if isinstance(self.capacity, int) == False or self.capacity < 0:
+            raise ValueError
+
+    def __str__(self):
+        return self._size * '🍪'
+
+    def deposit(self, n):
+        self.size += n
+        if self.size > self.capacity:
+            raise ValueError
+
+    def withdraw(self, n):
+        if n > self.size:
+            raise ValueError
+        else:
+            self.size -= n
+
+    @property
+    def capacity(self):
+         return self.capacity
+    # self._capacity?
+
+    @property
+    def size(self):
+        return self.size
+    # self._size?
+
+
+def main():
+
+    jar = Jar(10)
+
+    jar.size = 8
+    print(str(jar))
+
+    jar.deposit(2)
+    print(str(jar))
+
+    jar.withdraw(1)
+    print(str(jar))
+
+
+
+main()
